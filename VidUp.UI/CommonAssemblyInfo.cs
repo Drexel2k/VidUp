c856a009b29bd793ecdfebdbdf8f1384353e6d9e
Drexel2k <13720303+Drexel2k@users.noreply.github.com>
2021-01-01 15:31:16 +0100

    New version.

--- VidUp.UI/CommonAssemblyInfo.cs
+++ VidUp.UI/CommonAssemblyInfo.cs
@@ -3,10 +3,10 @@ using System.Runtime.InteropServices;
 [assembly: System.Reflection.AssemblyProductAttribute("VidUp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VidUp")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Drexel Development")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.4.0.1")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.5.0.0")]
 //Shown in Gui/Setup
-[assembly: System.Reflection.AssemblyVersionAttribute("1.4.0.1")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.4.0.1")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.5.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.5.0.0")]
 [assembly: System.Reflection.AssemblyCopyright("©2020 Drexel Development")]
 [assembly: ComVisible(false)]
 
