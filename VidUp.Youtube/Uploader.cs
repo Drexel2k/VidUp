eba4759e021df177a78ab3e79db05577e2b655de
Drexel2k <13720303+Drexel2k@users.noreply.github.com>
2021-08-17 13:07:01 +0200

    New version 1.9.2

--- VidUp.Youtube/Uploader.cs
+++ VidUp.Youtube/Uploader.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Timers;
 using Drexel.VidUp.Business;
 using Drexel.VidUp.Json.Content;
 using Drexel.VidUp.Utils;
