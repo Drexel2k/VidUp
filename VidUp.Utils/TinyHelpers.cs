96ecd9f93cff210916984df7d332745df8531f2e
Drexel2k <13720303+Drexel2k@users.noreply.github.com>
2022-05-30 15:33:48 +0200

    Quota help text improved.

--- VidUp.Utils/TinyHelpers.cs
+++ VidUp.Utils/TinyHelpers.cs
@@ -4,7 +4,7 @@ namespace Drexel.VidUp.Utils
 {
     public class TinyHelpers
     {
-        public static string QuotaExceededString = "VidUp's YouTube access is closed for today (API quota exceeded). Try again after midnight pacific time (UTC-8).";
+        public static string QuotaExceededString = "VidUp's YouTube access is limited to a few requests per minute, try again in one minute.\n Also total requests per day are limitted, if the problem persists, API access may also be completely closed for today (API quota exceeded). Try again after midnight pacific time (UTC-8).";
         public static string AuthenticationErrorString = "Authentication denied. If the error is permanent, maybe try re-sign in under Settings->YouTube Account->Kebab Menu.";
 
         public static string TrimLineBreakAtEnd(string text)
